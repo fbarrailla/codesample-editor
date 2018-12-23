d1b314d59afd48c9fb62da6f476d900897ccac39
François Barrailla <francois.barrailla@fullsix.com>
2018-12-23 14:29:14 +0700

    Updates

---
+++ precache-manifest.790f0abedecb80e6b14534acbca59559.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "35110a38840277157a16",
+    "url": "/codesample-editor/static/js/runtime~main.35110a38.js"
+  },
+  {
+    "revision": "d69eb9b41c91991bf2ad",
+    "url": "/codesample-editor/static/js/main.d69eb9b4.chunk.js"
+  },
+  {
+    "revision": "fbd9261f142b8de64fe9",
+    "url": "/codesample-editor/static/js/1.fbd9261f.chunk.js"
+  },
+  {
+    "revision": "f644406a9a219c4d19cff888ed6d677f",
+    "url": "/codesample-editor/index.html"
+  }
+];
